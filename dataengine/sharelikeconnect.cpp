cf30a29cfdff6bc4943edba5f70788b956b1251a
Marco Martin <notmart@gmail.com>
2011-05-30 19:57:00 +0200

    skeleton for the share-like-connect dataengine

---
+++ dataengine/sharelikeconnect.cpp
@@ -0,0 +1,45 @@
+/*
+ *  Copyright 2011 Aaron Seigo <aseigo@kde.org>
+ *  Copyright 2011 Marco Martin <mart@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU Library General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or (at your
+ *  option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+ *  License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to the
+ *  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ *  02110-1301, USA.
+ */
+
+#include "sharelikeconnect.h"
+
+#include "sharecontainer.h"
+
+
+ShareLikeConnectEngine::ShareLikeConnectEngine(QObject *parent, const QVariantList &args)
+    : Plasma::DataEngine(parent, args)
+{
+    m_shareContainer = new ShareContainer(this);
+    m_shareContainer->setObjectName("Share");
+    addSource(m_shareContainer);
+}
+
+bool ShareLikeConnectEngine::sourceRequestEvent(const QString &source)
+{
+    return (source == "Share" || source == "Like" || source == "Connect");
+}
+
+
+// export the plugin; use the plugin name and the class name
+K_EXPORT_PLASMA_DATAENGINE(org.kde.sharelikeconnect, ShareLikeConnectEngine)
+
+// include the moc file so the build system makes it for us
+#include "sharelikeconnect.moc"
+
