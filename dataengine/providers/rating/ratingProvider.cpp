d468de1f0d25e58625921b5c6eb606851ea9fd76
Marco Martin <notmart@gmail.com>
2011-06-07 12:12:02 +0200

    add copyright

--- dataengine/providers/rating/ratingProvider.cpp
+++ dataengine/providers/rating/ratingProvider.cpp
@@ -1,5 +1,6 @@
 /*
  *   Copyright 2011 Aaron Seigo <aseigo@kde.org>
+ *   Copyright 2011 Marco Martin <mart.org>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License as
